c7f9a0b2389c32b0bae7389eeb5e6106ffd19e59
Adler Santos <adler.coelho.santos@icloud.com>
2024-01-13 09:39:17 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <SideNav />
+    </div>
+    <div class="column is-three-quarter">ww</div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import SideNav from "./components/SideNav.vue";
 
-export default {
-  name: 'App',
+export default defineComponent({
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SideNav,
+  },
+});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
